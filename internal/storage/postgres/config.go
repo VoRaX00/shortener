e40b43bb0eaa9396e57649427483718ca001afd3
nikita <nikitakerzhik@gmail.com>
2025-03-09 11:09:00 +1000

    upd

--- internal/storage/postgres/config.go
+++ internal/storage/postgres/config.go
@@ -4,7 +4,7 @@ type Config struct {
 	Host     string `yaml:"host"`
 	Port     int    `yaml:"port"`
 	Username string `yaml:"user"`
-	Password string
+	Password string `yaml:"password"`
 	Database string `yaml:"dbname"`
 	SSLMode  string `yaml:"ssl_mode"`
 }
